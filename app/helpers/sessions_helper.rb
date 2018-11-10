eea77c8291d267bfd1c8b203c3f315cb80c40f3c
Mutuku Kyalo <28805113+jmutuku95@users.noreply.github.com>
2018-11-10 21:05:23 +0300

    Add remember me checkbox

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,15 +3,24 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def log_in_user_from_cookies(user)
+    if user && user.authenticated?(cookies[:remember_token])
+      log_in(user)
+      @current_user = user
+    end
+  end
+
+  def get_user_id_from_cookies
+    cookies.signed[:user_id]
+  end
+
+  # get current user from session or cookies if remember_me is chosen
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find(user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find(user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in(user)
-        @current_user = user
-      end
+      log_in_user_from_cookies(user)
     end
   end
 
