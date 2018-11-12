4c6b2a580e1b62e8c054735774b0f1599d1f146c
Charles Chuck <4069319+chalchuck@users.noreply.github.com>
2018-11-12 09:32:52 +0300

    Update app/models/user.rb
    Co-Authored-By: jmutuku95 <28805113+jmutuku95@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,7 +47,7 @@ class User < ApplicationRecord
   end
 
   def send_activation_email
-    UserMailer.account_activation(self).deliver_now
+    UserMailer.account_activation(self).deliver_later
   end
 
   private
