6112aabf18ee2b9dda07d75dfaddf696ed617c55
Mutuku Kyalo <28805113+jmutuku95@users.noreply.github.com>
2018-12-12 16:16:07 +0300

    Go back to sendgrid on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,13 +88,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'vast-hamlet-38373.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: Rails.application.credentials.gmail_username,
-    password: Rails.application.credentials.gmail_password,
-    authentication: 'plain',
-    domain: 'vast-hamlet-38373.herokuapp.com',
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    # authentication: plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
     enable_starttls_auto: true
   }
   config.require_master_key = true
